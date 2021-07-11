dead5555d2ae51a1856dd92c9b32253a4d760cf2
Lucas Lima <lucaslimapoa3@gmail.com>
2021-07-11 21:54:32 +0200

    Initial Commit

---
+++ MovieDB-SwiftRedux/MovieDB_SwiftReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDB_SwiftReduxApp.swift
+//  MovieDB-SwiftRedux
+//
+//  Created by Lucas Lima on 11.07.21.
+//
+
+import SwiftUI
+
+@main
+struct MovieDB_SwiftReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
